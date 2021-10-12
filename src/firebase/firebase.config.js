fd18bdde16c9d8d90e1cbd5788373f5d330baeac
saddaul-siam <saddadul.siam@gmail.com>
2021-10-12 12:11:15 +0600

    added all file

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAByo93gZy6xXCP4IQDTo9hi0A3rXGX7qs",
+  authDomain: "burj-al-arab-ec7bf.firebaseapp.com",
+  projectId: "burj-al-arab-ec7bf",
+  storageBucket: "burj-al-arab-ec7bf.appspot.com",
+  messagingSenderId: "871595525122",
+  appId: "1:871595525122:web:a69ff331f59e88788bd31b"
+};
+
+export default firebaseConfig;
